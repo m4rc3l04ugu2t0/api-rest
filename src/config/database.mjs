54b3bbcd0a6d9948e5bd1df6ee73e1fb1d0d65bb
Marcelo <mrcelo014@gmail.com>
2024-04-04 00:16:59 -0300

    seeds

---
+++ src/config/database.mjs
@@ -0,0 +1,74 @@
+import 'dotenv/config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname, ' - ', __filename)
+
+export default {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mariadb",
+    define: {
+      timestamps: 'America/Sao_Paulo',
+      underscored: true,
+      underscoredAll: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    dialectOptions: {
+      timezone: 'America/Sao_Paulo'
+    },
+    timezone: 'America/Sao_Paulo',
+    modelsPath: resolve(__dirname, 'src', 'models'),
+    migrationsPath: resolve(__dirname, 'src', 'database', 'migrations'),
+    seedersPath: resolve(__dirname, 'src', 'database', 'seeds')
+  },
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mariadb",
+    define: {
+      timestamps: 'America/Sao_Paulo',
+      underscored: true,
+      underscoredAll: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    dialectOptions: {
+      timezone: 'America/Sao_Paulo'
+    },
+    timezone: 'America/Sao_Paulo',
+    modelsPath: resolve(__dirname, 'src', 'models'),
+    migrationsPath: resolve(__dirname, 'src', 'database', 'migrations'),
+    seedersPath: resolve(__dirname, 'src', 'database', 'seeds')
+  },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mariadb",
+    define: {
+      timestamps: 'America/Sao_Paulo',
+      underscored: true,
+      underscoredAll: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    dialectOptions: {
+      timezone: 'America/Sao_Paulo'
+    },
+    timezone: 'America/Sao_Paulo',
+    modelsPath: resolve(__dirname, 'src', 'models'),
+    migrationsPath: resolve(__dirname, 'src', 'database', 'migrations'),
+    seedersPath: resolve(__dirname, 'src', 'database', 'seeds')
+  }
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,26 @@
 import { Sequelize } from "sequelize";
-import databaseConfig from '../config/database.cjs'
+import databaseConfig from '../config/database.mjs'
 import Student from "../models/Students.js";
 import User from "../models/Users.js";
 
 const models = [Student, User]
 
-const connection = new Sequelize(databaseConfig)
+const connection = new Sequelize(databaseConfig.development.database, databaseConfig.development.username, databaseConfig.development.password, {
+  host: databaseConfig.development.host,
+  dialect: 'mariadb', // ou substitua 'mariadb' pelo dialeto correspondente se estiver usando outro banco de dados
+  define: {
+    timestamps: databaseConfig.development.define.timestamps,
+    underscored: databaseConfig.development.define.underscored,
+    underscoredAll: databaseConfig.development.define.underscoredAll,
+    createdAt: databaseConfig.development.define.createdAt,
+    updatedAt: databaseConfig.development.define.updatedAt
+  },
+  dialectOptions: {
+    timezone: databaseConfig.development.dialectOptions.timezone
+  },
+  timezone: databaseConfig.development.timezone
+});
 
 models.forEach(model => {
   model.init(connection)
-})
+});
